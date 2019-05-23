f524bfe5944ccd53d8ad2f87859d7c7ce5e497d3
Alejandro <soniccrest34@gmail.com>
2019-05-23 10:53:18 -0500

    server

---
+++ gestion-server/server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'))
+
+app.use('/', routes)
+
+const port = process.env.PORT || 3000
+app.listen(port, (err) => {
+  if (err) throw err
+  console.log('server runnig  http://localhost:3000/')
+})
