4326c3f732f2c13a569fc25604c9e8eba38e7843
Alejandro <soniccrest34@gmail.com>
2019-05-24 17:28:34 -0500

    database connection

--- gestion-server/server.js
+++ gestion-server/server.js
@@ -3,10 +3,18 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 const path = require('path')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
