cab28c0dfb87a26a649b99ba9b67fb9c8833f8c3
Alejandro <soniccrest34@gmail.com>
2019-05-24 21:01:44 -0500

    db

--- gestion-server/routes.js
+++ gestion-server/routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const db = require('gestionbd')
+const db = require('gestion-db')
 
 router.get('/', (req, res) => {
   res.render('pages/index')
--- gestion-server/server.js
+++ gestion-server/server.js
@@ -11,10 +11,16 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json())
+app.use(session({
+  secret: 'cats',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.static('public'))
 
