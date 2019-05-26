4cf2de0411f2108487a4e64bef14cd39bfefbe96
David Viuche <jviucheroja@uniminuto.edu.co>
2019-05-26 00:33:59 -0500

    Paginas principales residente y admin :3

--- gestion-server/routes.js
+++ gestion-server/routes.js
@@ -16,6 +16,14 @@ router.get('/registro', (req, res) => {
   res.render('pages/registro')
 })
 
+router.get('/Admin', (req, res) => {
+  res.render('pages/indexAdmin')
+})
+
+router.get('/Residente', (req, res) => {
+  res.render('pages/indexUser')
+})
+
 router.post('/registro', async (req, res) => {
   await db.query('INSERT INTO propietarios (contraseña, nombres, apellidos, telefono, celular, correo, userName) VALUES ( ?, ?, ?, ?, ?, ?, ?)',
     [req.body.contraseña, req.body.nombres, req.body.apellidos, req.body.telefono, req.body.celular, req.body.correo, req.body.userName], (err, result) => {
